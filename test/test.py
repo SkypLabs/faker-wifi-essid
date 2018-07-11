c448a4ba92f48747775a136ec973d1908d1e2688
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2018-07-11 23:25:02 +0100

    add tests

--- test/test.py
+++ test/test.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+import re
 import unittest
 
 from faker import Faker
@@ -11,5 +12,21 @@ class TestWifiESSID(unittest.TestCase):
         self.fake.add_provider(WifiESSID)
 
     def test_if_string(self):
-        essid = self.fake.wifi_essid()
-        self.assertTrue(isinstance(essid, str))
+        for i in range(10):
+            self.assertTrue(isinstance(self.fake.wifi_essid(), str))
+
+    def test_common_essid(self):
+        for i in range(10):
+            self.assertTrue(self.fake.common_essid() in WifiESSID.common_essids)
+
+    def test_upc_default_essid(self):
+        r = re.compile("^UPC\d{7}$")
+
+        for i in range(10):
+            self.assertTrue(re.match(r, self.fake.upc_default_essid()))
+
+    def test_bbox_default_essid(self):
+        r = re.compile("^Bbox-[A-F0-9]{6}$")
+
+        for i in range(10):
+            self.assertTrue(re.match(r, self.fake.bbox_default_essid()))
