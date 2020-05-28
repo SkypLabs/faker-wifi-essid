48e7473debe2438c840e07ec4335c5a721ee733b
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2020-05-28 10:31:46 +0100

    Use double quotes in 'setup.py'

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+"""
+Setuptools build system configuration file
+for Faker Wi-Fi ESSID.
+
+See https://setuptools.readthedocs.io.
+"""
+
 from codecs import open as fopen
 from os.path import dirname, abspath, join
 from setuptools import setup, find_packages
@@ -9,34 +16,34 @@ VERSION = "0.2.0"
 
 DIR = dirname(abspath(__file__))
 
-with fopen(join(DIR, 'README.rst'), encoding='utf-8') as f:
+with fopen(join(DIR, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name = 'faker_wifi_essid',
+    name = "faker_wifi_essid",
     version = VERSION,
-    description = 'Faker provider for Wi-Fi ESSIDs.',
+    description = "Faker provider for Wi-Fi ESSIDs.",
     long_description = long_description,
-    license = 'MIT',
-    keywords = 'faker faker-library faker-provider faker-generator wifi essid',
-    author = 'Paul-Emmanuel Raoul',
-    author_email = 'skyper@skyplabs.net',
-    url = 'https://github.com/SkypLabs/faker-wifi-essid',
-    download_url = 'https://github.com/SkypLabs/faker-wifi-essid/archive/v{0}.zip'.format(VERSION),
+    license = "MIT",
+    keywords = "faker faker-library faker-provider faker-generator wifi essid",
+    author = "Paul-Emmanuel Raoul",
+    author_email = "skyper@skyplabs.net",
+    url = "https://github.com/SkypLabs/faker-wifi-essid",
+    download_url = "https://github.com/SkypLabs/faker-wifi-essid/archive/v{0}.zip".format(VERSION),
     classifiers = [
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'License :: OSI Approved :: MIT License',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT License",
     ],
-    packages = find_packages(exclude=['docs', 'tests', 'tests.*']),
-    test_suite = 'test',
+    packages = find_packages(exclude=["docs", "tests", "tests.*"]),
+    test_suite = "test",
     install_requires = [
-        'Faker',
+        "Faker",
     ],
 )
