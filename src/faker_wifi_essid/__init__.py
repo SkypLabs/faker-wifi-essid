8e7b1a33aa607d725a2476d3f41970d23d554f7e
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2024-11-27 14:14:24 +0100

    Replace 'setup.cfg' with 'pyproject.toml'
    This commit implements PEP 621 by specifying all project metadata in the
    'pyproject.toml' file.
    
    See https://peps.python.org/pep-0621/.

--- src/faker_wifi_essid/__init__.py
+++ src/faker_wifi_essid/__init__.py
@@ -4,11 +4,9 @@
 Faker Wi-Fi ESSID provider.
 """
 
-from pkg_resources import get_distribution
-
 from .wifi_essid import WifiESSID
 
-__version__ = get_distribution("faker_wifi_essid").version
+__version__ = "0.4.1"
 
 __all__ = [
     "WifiESSID",
